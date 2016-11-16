4d593c0a1524d4f712390e3a47916ec6eb5f41d3
Aki.SHIMO <fezrestia@gmail.com>
2016-11-16 20:18:13 +0900

    ADD: Initial frame project

---
+++ app/src/main/java/com/fezrestia/android/cloudsyncclient/RootActivity.java
@@ -0,0 +1,15 @@
+package com.fezrestia.android.cloudsyncclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RootActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+}
